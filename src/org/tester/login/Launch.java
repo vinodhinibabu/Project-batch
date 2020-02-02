f0b90c80298cd00e58097c7355f069abe49ac5ca
vinodhinibabu <vinodhinibabu19@gamil.com>
2020-02-02 10:31:21 +0530

    code done

--- src/org/tester/login/Launch.java
+++ src/org/tester/login/Launch.java
@@ -14,9 +14,25 @@ public class Launch {
 public static void main(String[] args) {
 	System.setProperty("webdriver.chrome.driver", "C:\\Users\\COM\\eclipse-workspace\\selenium\\driver\\chromedriver.exe");
 WebDriver driver=new ChromeDriver();
-driver.get("https://www.toolsqa.com/automation-practice-form/");
-WebElement con = driver.findElement(By.id("continentsmultiple"));
-Select s=new driver(con);
-System.out.println("123");
+//driver.get("https://www.toolsqa.com/automation-practice-form/");
+//WebElement con = driver.findElement(By.id("continentsmultiple"));
+Select s=new Select(con);
+
+//s.selectByValue("AF");
+s.selectByVisibleText("Europe");
+//s.selectByIndex(2);
+
+//List<WebElement> op=s.getOptions();
+//System.out.println(op.size());
+//for(int i=0; i<op.size(); i++) 	
+//{
+	//WebElement x=op.get(i);
+	//String txt=x.getText();
+	//System.out.println(txt);
+//}
+//for(WebElement x:op) {
+	//String at=x.getAttribute("value");
+//System.out.println(at);
+//}
 }
 }
