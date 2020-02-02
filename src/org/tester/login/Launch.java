35bdd51e90cffdd7e062dd2591afe6f3b4807624
vinodhinibabu <vinodhinibabu19@gamil.com>
2020-02-02 10:28:51 +0530

    code completed

--- src/org/tester/login/Launch.java
+++ src/org/tester/login/Launch.java
@@ -16,24 +16,7 @@ public static void main(String[] args) {
 WebDriver driver=new ChromeDriver();
 driver.get("https://www.toolsqa.com/automation-practice-form/");
 WebElement con = driver.findElement(By.id("continentsmultiple"));
-//Select s=new Select(con);
-System.out.println("code");
-
-//s.selectByValue("AF");
-//s.selectByVisibleText("Europe");
-//s.selectByIndex(2);
-
-//List<WebElement> op=s.getOptions();
-//System.out.println(op.size());
-//for(int i=0; i<op.size(); i++) 	
-//{
-	//WebElement x=op.get(i);
-	//String txt=x.getText();
-	//System.out.println(txt);
-//}
-//for(WebElement x:op) {
-	//String at=x.getAttribute("value");
-//System.out.println(at);
-//}
+Select s=new driver(con);
+System.out.println("123");
 }
 }
